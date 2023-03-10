fdb041b2724133cf7ba0846616bd38b03036dc41
PhucNguyen <phuche53@fsoft.com.vn>
2023-03-10 16:02:04 +0700

    finish main layout, video minute: 50:56

--- app/src/main/java/com/example/prmproject/MainActivity.java
+++ app/src/main/java/com/example/prmproject/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.prmproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.prmproject.domain.CategoryDomain;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView.Adapter adapter;
+    private RecyclerView recyclerViewCategoryList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerViewCategory();
     }
+
+    private void recyclerViewCategory(){
+        LinearLayoutManager linearLayoutManager =  new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
+        recyclerViewCategoryList = findViewById(R.id.recyclerView);
+        recyclerViewCategoryList.setLayoutManager(linearLayoutManager);
+
+        ArrayList<CategoryDomain> catagory = new ArrayList<>();
+        catagory.add(new CategoryDomain("Pizze","cat_1"));
+        catagory.add(new CategoryDomain("Burger","cat_2"));
+        catagory.add(new CategoryDomain("Hot Dog","cat_3"));
+        catagory.add(new CategoryDomain("Drink","cat_4"));
+        catagory.add(new CategoryDomain("Donut","cat_5"));
+
+    }
+
+
 }
---
+++ app/src/main/java/com/example/prmproject/adaptor/CategoryAdaptor.java
@@ -0,0 +1,42 @@
+package com.example.prmproject.adaptor;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CategoryAdaptor extends RecyclerView.Adapter<CategoryAdaptor.ViewHolder> {
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CategoryAdaptor.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView categoryName;
+        ImageView categoryPic;
+        ConstraintLayout mainLayout;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
+
+
